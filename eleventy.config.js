c5bf6223822caa8ba0a7c7404e2cfe212a48a86a
omar-hossam <omarhossam50@proton.me>
2026-05-08 15:21:12 +0300

    feat: connect decap CMS, implement starter

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,27 +1,16 @@
-import tailwindcss from "@tailwindcss/postcss";
-import postcss from "postcss";
-import fs from "fs";
-
 export default function (eleventyConfig) {
-  // 1. Passthrough: Move icons and admin to build
-  // eleventyConfig.addPassthroughCopy({ "src/icons": "icons" });
-  // eleventyConfig.addPassthroughCopy("admin");
-
-  // 2. Tailwind Processing
-  eleventyConfig.on("eleventy.before", async () => {
-    const css = fs.readFileSync("./src/css/style.css", "utf8");
-    const result = await postcss([tailwindcss]).process(css, { from: "./src/css/style.css" });
-    fs.mkdirSync("./_site/css", { recursive: true });
-    fs.writeFileSync("./_site/css/style.css", result.css);
-  });
+  eleventyConfig.addPassthroughCopy({ "src/images": "images" });
+  eleventyConfig.addPassthroughCopy({ "src/css": "css" });
+  eleventyConfig.addPassthroughCopy({ "src/js": "js" });
+  eleventyConfig.addPassthroughCopy("admin");
 
   return {
     dir: {
       input: "src/content",  // Your source files live here
       output: "_site",
-      // These are relative to the 'input' directory (src/content)
       includes: "../../_includes", 
-      data: "../../_data"
+      data: "../../_data",
+      components: "../../_components"
     }
   };
 };
---
+++ src/content/products/cheese-burger.md
@@ -0,0 +1,6 @@
+---
+title: Cheese burger
+price: 12.45
+image: /images/products/paper-plane-tilt-1-.svg
+---
+* Nice!
